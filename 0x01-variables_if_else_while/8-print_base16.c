39f702b369be68515b6e0c1e05a8a73ab3c71e3c
ekarkat <ekarkat@gmail.com>
2023-03-17 16:23:52 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point Printing alphabet
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 48;
+	while (a <= 122)
+	{
+		if (a == 57)
+			a = 97;
+		if (a == 102)
+			break;
+		putchar(a);
+		a = a + 1;
+	}
+	putchar('\n');
+	return (0);
+}
