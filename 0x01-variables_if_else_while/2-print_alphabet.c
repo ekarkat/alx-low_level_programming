abee378c42c9fabb8e535fcebf33e3d141f0a4c9
ekarkat <ekarkat@gmail.com>
2023-03-17 10:23:56 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point Printing alphabet
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 97;
+	while (a = < 122)
+	{
+		putchar(a);
+		a = a + 1;
+	}
+}
