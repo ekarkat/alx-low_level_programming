6067ea27b94ad9ebe54e4c7c3a2786ae00b3eac9
ekarkat <ekarkat@gmail.com>
2023-03-17 15:29:57 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point Printing alphabet
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a, b;
+
+	a = 97;
+	b = 65;
+	while (a <= 122) /*lower case*/
+	{
+		putchar(a);
+		a = a + 1;
+	}
+	while (b <= 90) /* UPPER CASE */
+	{
+		putchar(b)
+	}
+	putchar('\n');
+	return (0);
+}
