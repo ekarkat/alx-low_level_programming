c838da9720cec87589258d8f6582311e0e6fb229
ekarkat <ekarkat@gmail.com>
2023-03-18 08:38:24 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,10 @@ int main(void)
 			d1 = d3;
 			while (d1 <= 57)
 			{
+				if (d1 == d3)
 				d0 = d2 + 1;
+				else
+				d0 = 48;
 				while (d0 <= 57)
 				{
 					putchar(d3);
@@ -25,17 +28,17 @@ int main(void)
 					putchar(32);
 					putchar(d1);
 					putchar(d0);
-					if ( d3 == 57 && d2 == 56 && d1 == 57 && d0 == 57)
-                                		break;                        
+					if (d3 == 57 && d2 == 56 && d1 == 57 && d0 == 57)
+						break;
 					putchar(44);
 					putchar(32);
-					d0 = d0 + 1; 
+					d0 = d0 + 1;
 				}
-				d1 = d1 +1;                        
+				d1 = d1 + 1;
 			}
-			d2 = d2 + 1;            
+			d2 = d2 + 1;
 		}
-		d3 = d3 +1;
+		d3 = d3 + 1;
 	}
 	putchar('\n');
 	return (0);
