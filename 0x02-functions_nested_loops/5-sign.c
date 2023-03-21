480ae89df3f8f881f54b207c702953e5a23e426a
ekarkat <ekarkat@gmail.com>
2023-03-21 13:49:25 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - Check if c is an alphabet
+ * @n: the variable
+ * Return: 1 if positive 0 if zero (-1) if negative
+ */
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	else if (n == 48)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('+');
+		return (1);
+	}
+}
