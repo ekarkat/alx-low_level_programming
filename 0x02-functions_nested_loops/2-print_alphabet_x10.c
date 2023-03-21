c92509d288e39aef34e8945dcd6a58ffdeebc6f9
ekarkat <ekarkat@gmail.com>
2023-03-21 13:20:29 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Program that Print alphabet 10 times
+ * Return: return 0
+ */
+void print_alphabet_x10(void)
+{
+	int a, n;
+
+	n = 1;
+	while (n <= 10)
+	{
+		a = 97;
+		while (a <= 122)
+		{
+			_putchar(a);
+			a = a + 1;
+		}
+		n = n + 1;
+	}
+	_putchar('\n');
+}
