a73d2ae772e119b14560e760651ba2c00a240a0a
ekarkat <ekarkat@gmail.com>
2023-03-21 16:52:00 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Add two integers and returns result
+ * @a: first number
+ * @b: secand nummber
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int rs;
+
+	rs = a + b;
+
+	return (rs);
+}
