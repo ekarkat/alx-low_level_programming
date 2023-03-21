3a214a323db5163853581f13fc77953ed491de30
ekarkat <ekarkat@gmail.com>
2023-03-21 14:10:08 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * * @n: the variable
+ * Return: none
+ */
+
+int _abs(int n)
+{
+	int b;
+
+	if (n < 0)
+	{
+		b = -a;
+		_putchar(b);
+	}
+	else if (n == 0)
+	{
+		_putchar(48)
+	}
+	else
+	{
+		_putchar(a);
+	}
+}
