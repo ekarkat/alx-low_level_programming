351d298345dee531d7b3ee6ab2b415a41a260dcf
ekarkat <ekarkat@gmail.com>
2023-03-21 15:10:12 -0700

    task 7.5

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,18 @@
 int print_last_digit(int n)
 {
 	int d0, abs;
-
-	abs = _abs(n);
-	d0 = (abs % 10) + '0';
+	
+	if (n < 0)
+	{
+		abs = -n;
+		d0 = (abs % 10) + '0';
+		_putchar(d0);
+		return (abs % 10);
+	}
+	else
+	{
+	d0 = (n % 10) + '0';
 	_putchar(d0);
-	return (abs % 10);
+	return (n % 10);
+	}
 }
