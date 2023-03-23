834f8a449714eac0c078e0ba21dc3954efafbbcb
ekarkat <ekarkat@gmail.com>
2023-03-23 13:28:25 -0700

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,9 +5,9 @@
  * @c: the char
  * Return: 1 if c is digit if else return 0
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
