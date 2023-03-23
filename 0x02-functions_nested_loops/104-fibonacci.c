e0647fe1322530b65aae589866356f92e172b0ee
ekarkat <ekarkat@gmail.com>
2023-03-22 19:19:36 -0700

    taskf

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+/**
+ * main - Fibonacci numbers
+ * Return: Always 0
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	long int i, k, n, m;
+
+	k = 1;
+	m = 1;
+	for (i = 1; i <= 98, i = i + 1)
+	{
+		printf("%lu", k);
+		if (i < 98)
+		printf(", ");
+		n = k;
+		k = k + m;
+		m = n;
+	}
+	printf("\n");
+	return (0);
+}
