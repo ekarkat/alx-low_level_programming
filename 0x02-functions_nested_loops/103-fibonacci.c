94b2e66b9e91272298dd856c1975bb47f5680d4f
ekarkat <ekarkat@gmail.com>
2023-03-22 19:11:31 -0700

    tals 023

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+/**
+ * main - Fibonacci numbers
+ * Return: Always 0
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	long int i, k, n, m;
+
+	k = 1;
+	m = 1;
+	for (; k <= 4000000;)
+	{
+		if ((k % 2) == 0)
+		{
+			i = i + k;
+			n = k;
+			k = k + m;
+			m = n;
+		}
+	}
+	printf("%lu\n, i");
+	return (0);
+}
