cd4009615213294f674ebeaaae83a7ae6d2d60b5
ekarkat <ekarkat@gmail.com>
2023-03-23 15:14:14 -0700

    task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,18 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
 printf("Fizz ");
-else if (i % 5 == 0 && i % 3 != 0)
+
+else if (i % 5 == 0 && i % 3 != 0 && i != 100)
 printf("Buzz ");
+
 else if (i % 3 == 0 && i % 5 == 0)
 printf("FizzBuzz ");
+
 else
 printf("%d ", i);
+if (i == 100)
+printf("Buzz");
 }
 printf("\n");
 return (0);
-
 }
