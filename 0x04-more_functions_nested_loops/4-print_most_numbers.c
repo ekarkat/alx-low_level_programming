35bdeba3c96cc1615845f7832f4ec1977856aae4
ekarkat <ekarkat@gmail.com>
2023-03-23 13:35:04 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i = i + 1)
+	{
+		if (i != '2' || i != '4')
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
