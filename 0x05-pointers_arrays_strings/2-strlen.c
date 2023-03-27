059a44d100bea3736f9f18a5c67876c1a5c84430
ekarkat <ekarkat@gmail.com>
2023-03-27 14:30:15 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: the string 
+ * Return: l
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	int l = 0;
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+
+	return (l);
+}
