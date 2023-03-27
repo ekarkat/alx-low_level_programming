79d5193baae00fe5188f41a3bcc5cc74be67d64f
ekarkat <ekarkat@gmail.com>
2023-03-27 15:58:04 -0700

    tas4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,18 +8,11 @@ void print_rev(char *s)
 	int i;
 
 	i = 0;
-	while (*s != '\0')
-	{
+	while (s[i] != '\0')
 		i++;
-		s++;
-	}	
-	i = i - 1;
-	while (i >= 0)
+	while (i > 0)
 	{
-		_putchar(*s);
-		s--;
-		i--;
+		_putchar(s[i]);
 	}
-		_putchar('\n');
+	_putchar('\n');
 }
-
