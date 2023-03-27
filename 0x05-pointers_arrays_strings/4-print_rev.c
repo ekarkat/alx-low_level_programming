19f2995bd40c67b32665229391081fb210614798
ekarkat <ekarkat@gmail.com>
2023-03-27 15:08:19 -0700

    task 4 modif

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,20 +3,20 @@
  * print_rev - prints a string in reverse
  * @str: string
  */
-void print_rev(char *str)
+void print_rev(char *s)
 {
 	int i;
 
 	i = 0;
-	while (*str != '\0')
+	while (*s != '\0')
 	{
 		i++;
-		str++;
+		*s++;
 	}	
 	i = i - 1;
 	while (i >= 0)
 	{
-		_putchar(str[i]);
+		_putchar(s[i]);
 	}
 		_putchar('\n');
 }
