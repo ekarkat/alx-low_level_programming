cbc3a54147666286d38a8e1f6bd6d3daeebfdb8b
ekarkat <ekarkat@gmail.com>
2023-03-30 16:57:32 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,12 @@ int _strcmp(char *s1, char *s2)
 	int i, df;
 
 	i = 0;
-	while (str1[i] == str2[i])
+	while (s1[i] == s2[i])
 	{
-		if (str1[i] == '\0')
+		if (s1[i] == '\0')
 			return (0);
 		i++;
 	}
-	df = (str1[i] - str2[i])
+	df = (s1[i] - s2[i])
 	return (df);
 }
