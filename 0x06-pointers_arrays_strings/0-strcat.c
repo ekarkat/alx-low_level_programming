7fe2746f01e5fc06ab6fd58391d6ea67c5f5f13d
ekarkat <ekarkat@gmail.com>
2023-03-30 15:43:51 -0700

    tas0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_stract - concatenates two strings
+ * @dest: first string
+ * @src: 2end string
+ * return: a pointer to the result dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
