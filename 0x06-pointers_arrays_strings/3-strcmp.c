f23698dc352c1acac43abf1fcab31de9615bb49e
ekarkat <ekarkat@gmail.com>
2023-03-30 18:09:50 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: diffrence between s1 and s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+
+}
