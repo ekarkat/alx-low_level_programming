0a53420f2bb78a5b80c369789a4d83b0e5eb42d1
ekarkat <ekarkat@gmail.com>
2023-03-30 17:41:00 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122
+			s[i] = s[i] - 97 + 65;
+	}
+
+	return (s)
+}
