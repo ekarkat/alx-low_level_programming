aab3bf5020e1c22ba6586c1b086b4ef75bf738b9
ekarkat <ekarkat@gmail.com>
2023-04-03 15:18:06 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+/**
+ **_memset - prints buffer in hexa
+ * @s: the address of memory to print
+ * @n: the size of the memory to print
+ * @b: the constant
+ * Return: Nothing.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+
+	return (s)
+}
