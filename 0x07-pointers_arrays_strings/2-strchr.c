a688ae3c48bfc5e099a328c7be6f84f5c2de656c
ekarkat <ekarkat@gmail.com>
2023-04-03 16:34:25 -0700

    ask 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,15 +12,11 @@ char *_strchr(char *s, char c);
 {
 	int i;
 
-	i = 0;
-	while (s[i] != '\0')
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
-		i++;
 	}
-	if (s[i] == c)
-		return (s + i);
 
 	return ('\0');
 }
