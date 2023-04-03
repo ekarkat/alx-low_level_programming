77f2580305277b3e3b2ca04462153b47cb7982ce
ekarkat <ekarkat@gmail.com>
2023-04-03 16:13:31 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,7 +9,6 @@
 char *_strchr(char *s, char c);
 {
 	int i;
-	char *pc;
 
 	i = 0;
 	while (s[i] != '\0')
@@ -21,6 +20,6 @@ char *_strchr(char *s, char c);
 
 	if (s[i] == c)
 		return (s[i]);
-	else
-		return ('\0');
+
+	return ('\0');
 }
