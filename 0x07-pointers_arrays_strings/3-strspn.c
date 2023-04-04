7bdc6166dd7576206300edc7979200d59cdf966f
ekarkat <ekarkat@gmail.com>
2023-04-03 18:43:09 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,3 +8,26 @@
 unsigned int _strspn(char *s, char *accept)
 
 {
+	int n;
+	int i;
+
+	n = 0;
+	while (s[j] != '\0')
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (s[j] == accept[i])
+			{
+				n++;
+				break;
+			}
+
+			else if (accept[i + 1] == '\0')
+				return (n);
+		}
+
+		j++;
+	}
+
+	return (n);
+}
