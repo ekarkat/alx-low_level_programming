16dba884b301bf3b7ce13ece499b43c88c9ace47
ekarkat <ekarkat@gmail.com>
2023-04-03 17:23:38 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - sum of the diagboles
+ * @a: the array matrix
+ * @size: matrix size
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, d1, d2;
+
+	i = 0;
+	d1 = 0;
+	d2 = 0;
+
+	for (; i < size; i++)
+	{
+		d1 = d1 + a[i][i];
+		d2 = d2 + a[size - i][size - i];
+	}
+
+	printf("%i, %i\n", sum1, sum2);
+}
