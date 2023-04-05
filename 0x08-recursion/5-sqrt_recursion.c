f4876ca462ebeccee3d326594574f8f105024a33
ekarkat <ekarkat@gmail.com>
2023-04-05 16:49:29 -0700

    task 6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * @n : the number
+ * Return: the square root
+ */
+
+int test(int i, int n)
+{
+	if ()
+}
+
+int _sqrt_recursion(int n)
+{
+	if ()
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * test - check if the number can be devided by i
+ * @n : is the number
+ * @i : the dev
+ * Return: 1 or 0
+ */
+
+int test(int n, int i)
+{
+
+	if (n % 2 == 0)
+		return (0);
+	if (n % i == 0 && n != i)
+		return (0);
+	else if (n % i != 0 && n != i)
+		return (test(n, (i + 2)));
+	else
+		return (1);
+}
+
+/**
+ * is_prime_number - check if n is a prime number
+ * @n : the number
+ * Return: 1 if the input integer is a prime number, otherwise return 0.
+ */
+
+
+int is_prime_number(int n)
+{
+	return (test(n, 3));
+}
