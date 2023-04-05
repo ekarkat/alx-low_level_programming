438091a684a22a2e63b551fd3f3aa7d93554495e
ekarkat <ekarkat@gmail.com>
2023-04-05 13:25:41 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,15 +8,12 @@
 
 void _puts_recursion(char *s)
 {
-	int i;
-
-	i = 0;
 	if (*s == '\0')
+	{
+		_putchar('\n');
 		return;
+	}
 	_putchar(*s);
 	s++;
-	i++;
 	_puts_recursion(s);
-	if (s[i] == '\0')
-		_putchar('\n');
 }
