ac4824b167c0f2d9662d00aa385fe11fd644c3fa
ekarkat <ekarkat@gmail.com>
2023-04-05 13:50:26 -0700

    tas 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursios - print reverse string
+ * @s : the string
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+	
+}
