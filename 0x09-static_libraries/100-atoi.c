f429311b644f1146dbdae1577dbed103c229063c
ekarkat <ekarkat@gmail.com>
2023-04-09 11:40:13 -0700

    at0

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -4,8 +4,26 @@
  * @s : string
  * Return: return 0
  */
-
 int _atoi(char *s)
 {
-	return (0);
+int sign = 1;
+int num = 0;
+
+if (*s == '-')
+{
+sign = -1;
+s++;
+}
+else if (*s == '+')
+{
+s++;
+}
+
+while (*s >= '0' && *s <= '9')
+{
+num = num * 10 + (*s - '0');
+s++;
+}
+int answ = num *sign;
+return (answ);
 }
