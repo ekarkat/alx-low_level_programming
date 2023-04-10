a687a61561b387fa3455880b3ab07f25fd741bde
ekarkat <ekarkat@gmail.com>
2023-04-10 13:45:13 -0700

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,11 @@
 /**
  * main - print program name
  * Return: Always 0
+ * @argc : argument numbers
+ * @argv : arguments entered
  */
 int main(int argc, char *argv[])
 {
 printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
