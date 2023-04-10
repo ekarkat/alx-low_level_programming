1a48757686430d04d7ae107f1ad8a6bca326a6bd
ekarkat <ekarkat@gmail.com>
2023-04-10 14:19:18 -0700

    task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - printing a program that prints arguments passed into
+ * @argc : number of arguments
+ * @argv : arguments passed
+ * Return: always 0
+ */
+
+int main (int argc, char *argv[])
+{
+	int i, n;
+
+	i = 0;
+	n = argc - 1;
+	while (i <= n)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
