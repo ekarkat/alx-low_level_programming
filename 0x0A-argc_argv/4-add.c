ea31fa25c3a8d02140452a2f221106f9292cfa60
ekarkat <ekarkat@gmail.com>
2023-04-10 15:29:34 -0700

    ask 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Write a program that sum positiv numbers.
+ * @argc: arguments number
+ * @argv: the arguments passed in
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int a, b, i, j;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	for (i = 1; i <= argc - 1; i++)
+	{
+		char *r = argv[i];
+		for (j = 0; r[j] != '\0'; j++)/*Check if argv[i] is a digit*/
+		{
+			int id = isdigit(r[j]);
+			if (id == 0)
+			{
+				printf("Errorr\n");
+				return (1);
+			}
+		}
+		a = atoi(argv[i]);
+		b = b + a;
+	}
+	printf("%d\n", b);
+	return (0);
+}
