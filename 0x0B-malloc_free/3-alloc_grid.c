d18d663175b58d21e8cae65e39aa284ce0bc3240
ekarkat <ekarkat@gmail.com>
2023-04-11 17:49:51 -0700

    tas

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * **alloc_grid - a function that returns a pointer to a 2
+ * dimensional array of integers.
+ * @width : width
+ * @height : the height
+ * Return: return pointer or null
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **matrix, i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	matrix = malloc(8 * height);
+
+	if (matrix == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		matrix[i] = malloc(4 * width);
+		for (j = 0; j < width; j++)
+			matrix[i][j] = 0;
+	}
+	return (matrix);
+}
