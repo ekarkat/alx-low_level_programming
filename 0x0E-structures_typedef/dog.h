0d74ffb771a1acea13961966aa6eddd2e9a1f7bf
ekarkat <ekarkat@gmail.com>
2023-04-17 14:43:16 -0700

    dog.h 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Anew type named dog
+ * @name : dog name
+ * @age : dog age
+ * @owner : dog owner
+ *
+ * Description : A new structer named dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
