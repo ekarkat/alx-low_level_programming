bd7c3621a8873819f29d557a41b2f5115e353864
ekarkat <ekarkat@gmail.com>
2023-04-20 14:37:51 -0700

    tas

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup - a function that returns a pointer to a newly allocated space in
+ * memory, which contains a copy of the string given as a parameter.
+ * @str : string
+ * Return: return pointer or null if memory is insufficient
+ */
+
+char *_strdup(char *str)
+{
+        char *newstr;
+        int i, ln;
+
+        if (str == NULL)
+                return (NULL);
+
+        for (ln = 0; str[ln] != '\0'; ln++)
+                ln++;
+        newstr = malloc(ln + 1);
+
+        if (newstr == NULL)
+                return (NULL);
+
+        for (i = 0; str[i] != '\0'; i++)
+        {
+                newstr[i] = str[i];
+        }
+
+        return (newstr);
+}
+
+/**
+ * *argstostr - concatenates all the arguments of your program.
+ * @ac : argc
+ * @av : argv
+ * Return: Returns a pointer to a new string, or NULL if it fails
+ */
+
+char *argstostr(int ac, char **av)
+{
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+}
