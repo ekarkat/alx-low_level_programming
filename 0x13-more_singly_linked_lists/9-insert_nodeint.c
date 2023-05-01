97c985d9415a719fc02b808c605e65b79a2cc997
ekarkat <ekarkat@gmail.com>
2023-05-01 22:39:44 +0100

    readme

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,64 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - add node at the beginning of the list
+ * @head: the address of head !!
+ * @n : the new node value of n "node->n"
+ * Return: new node address
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	else
+	{
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+	}
+
+	return (*head);
+}
+
+/**
+ * insert_nodeint_at_index - insert node at idx
+ * @head : head of the list
+ * @n : the data node->n
+ * @idx : index of new node
+ * Return: the address of the new node
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp;
+	listint_t *new_node;
+	unsigned int i = 0;
+
+	temp = *head;
+	if (*head == NULL)
+		return (NULL);
+
+	while (i < idx - 1 && temp != NULL)
+	{
+		if (idx == 0)
+			return (add_nodeint(head, n));
+		temp = temp->next;
+		i++;
+	}
+	if (i == idx - 1)
+	{
+		new_node = malloc(sizeof(listint_t));
+		new_node->n = n;
+		new_node->next = ((temp->next));
+		temp->next = new_node;
+		return (new_node);
+
+	}
+	else
+		return (NULL);
+}
