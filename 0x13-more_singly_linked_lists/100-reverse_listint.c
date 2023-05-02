691124962ebc41041f4757019cf1a8cd37beef2c
ekarkat <ekarkat@gmail.com>
2023-05-02 14:24:13 +0100

    task10

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * reverse_listint -  a function that reverses a listint_t linked list.
+ * @head : head of the list
+ * Return: return the address of the new list head
+*/
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next1, *next2;
+
+	/*next1 save the next node, next2 save the one after next node*/
+	next1 = (*head)->next;
+	next2 = next1->next;
+	/*setting the head of the list ->next to NULL*/
+	(*head)->next = NULL;
+	next1->next = *head;
+	*head = next1;
+	next1 = next2;
+	next2 = next1->next;
+	/*looping through the other nodes*/
+	while (next2 != NULL)
+	{
+		next1->next = *head;
+		*head = next1;
+		next1 = next2;
+		next2 = next1->next;
+	}
+	next1->next = *head;
+	*head = next1;
+	return (*head);
+}
---
+++ 0x13-more_singly_linked_lists/2-main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+
+    head = malloc(sizeof(listint_t));
+	head->n = 15;
+	head->next = NULL;
+
+    add_nodeint(&head, 0);
+    add_nodeint(&head, 1);
+    add_nodeint(&head, 2);
+    add_nodeint(&head, 3);
+    add_nodeint(&head, 4);
+    add_nodeint(&head, 98);
+    add_nodeint(&head, 402);
+    add_nodeint(&head, 1024);
+    print_listint(head);
+    return (0);
+}
