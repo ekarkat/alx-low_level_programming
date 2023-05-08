269fc36e28a9b43be172fb319d13f9c73330a8b3
ekarkat <ekarkat@gmail.com>
2023-05-08 22:59:55 +0100

    agt

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - append text at the end
+ * @filename: the fiel ame
+ * @text_content : the text contetnt
+ * Return: return 1 or -1
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fid, ln = 0, tr;
+
+	if (filename == NULL)
+		return (-1);
+	fid = open(filename, O_WRONLY | O_APPEND);
+	if (fid == -1)
+		return (-1);
+	
+	if (text_content == NULL)
+		text_content = "";
+	while (text_content[ln] != 0)
+		ln++;
+	tr = write(fid, text_content, ln);
+	if (tr == -1)
+		return (-1);
+	close(fid);
+	return(1);
+}
