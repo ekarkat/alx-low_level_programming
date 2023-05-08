3a246912995f9930954eb22a155b7210e24aea93
ekarkat <ekarkat@gmail.com>
2023-05-08 22:36:58 +0100

    tas 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,6 +10,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fid, tr;
+	int i = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -17,9 +18,11 @@ int create_file(const char *filename, char *text_content)
 	fid = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fid == -1)
 		return (-1);
-	if (text_content == 0)
-		return (1);
-	tr = write(fid, text_content, 100);
+	if (text_content == NULL)
+		text_content = "";
+	while (text_content[i] != 0)
+		i++;
+	tr = write(fid, text_content, i);
 	if (tr == -1)
 		return (-1);
 	close(fid);
